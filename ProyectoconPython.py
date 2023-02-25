b6cccd5295f982dfbbb9008788236def0bb99d37
JairoDH <123737760+JairoDH@users.noreply.github.com>
2023-02-25 19:17:49 +0100

    Delete ProyectoconPython.py

--- ProyectoconPython.py
+++
@@ -1,8 +0,0 @@
-# Mostrar los trabajadores con sus provincias.
-# Pedir número del trabajador y mostrar toda su información. 
-# Mostrar los datos de los trabajadores pidiendo la matricula del camion
-# inserción de datos de un nuevo camión pidiendo la matricula, fecha de alta y peso máximo.
-# Pedir matricula para eliminar el camión.
-# pedir nombre del trabajador para actualizar el número telefono y el municipio.
-
-
